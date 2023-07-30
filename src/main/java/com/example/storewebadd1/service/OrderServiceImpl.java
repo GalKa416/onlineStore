03ee91af2f047bf60fe87ccf1fd74934946afb99
Галина <g416@mail.ru>
2023-07-30 16:52:18 +0500

    дз готова

--- src/main/java/com/example/storewebadd1/service/OrderServiceImpl.java
+++ src/main/java/com/example/storewebadd1/service/OrderServiceImpl.java
@@ -9,16 +9,19 @@ public class OrderServiceImpl implements OrderService {
     private final Cart cart;
 
     public OrderServiceImpl(Cart cart) {
+
         this.cart = cart;
     }
 
     @Override
     public List<Integer> addItems(List<Integer> itemsIds) {
+
         return cart.addItems(itemsIds);
     }
 
     @Override
     public List<Integer> getItems() {
+
         return cart.getItems();
     }
 }
